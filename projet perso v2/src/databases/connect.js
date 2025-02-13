b27cb3f440e077d77e40fe0624a6331de26c51c6
Brandon-KWP <brandon.dieu.17@gmail.com>
2025-02-13 15:01:46 +0100

    v3

--- projet perso v2/src/databases/connect.js
+++ projet perso v2/src/databases/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDb() {
   try {
-    await mongoose.connect(process.env.MONGO_URI + process.env.DB_NAME);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("database conncecté....");
   } catch (err) {
     console.log(err);
